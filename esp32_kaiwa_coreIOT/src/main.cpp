2d3c539f5d8c0ca17f0f7359b914e21a935c80df
Hieu031 <nth280102@gmail.com>
2025-03-12 15:05:26 +0700

    update

--- esp32_kaiwa_coreIOT/src/main.cpp
+++ esp32_kaiwa_coreIOT/src/main.cpp
@@ -2,6 +2,15 @@
 #include <PubSubClient.h>
 #include <ESP32Servo.h>
 #include <DHT.h>
+
+#define RELAY_PIN 26
+#define BUZZER_PIN 27
+
+//init functions
+void checkTemperature();
+void sendTelemetryData();
+void sendPumpStatus();
+
 // Cấu hình WiFi
 constexpr char WIFI_SSID[] = "Wokwi-GUEST";
 constexpr char WIFI_PASSWORD[] = "";  // Wokwi WiFi không cần mật khẩu
@@ -21,13 +30,23 @@ constexpr int Servo_PIN = 4;
 constexpr int DHT_PIN = 5;
 constexpr int DHT_TYPE = DHT22;
 
+//Temperature warning
+const float TEMP_THRESHOLD = 35.0; //turn on warning
+const float TEMP_HYSTERESIS = 33.0; //turn off pump
+
 //status devices
+bool pumpControl = false;
+bool pumpstate = false;
 bool ledstate = false;
 int servoAngle = 0; //rotation angle of servo
 
-//Frequence send data 5 seconds
+//Frequence send data 2 seconds
 const int TELEMETRY_SEND_INTERVAL = 2000;
 unsigned long lastSendTime = 0;
+//buzzer sound 1s
+unsigned long buzzerStartTime = 0;
+const int BUZZER_SOUND = 1000;
+bool isBuzzerOn = false;
 
 //object Servo & DHT22
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -62,7 +81,22 @@ void callback(char* topic, byte* payload, unsigned int length){
   Serial.println(message);
 
   int request_id = atoi(strrchr(topic, '/') + 1); //get request ID
-  
+  //control pump
+  if(message.indexOf("\"method\":\"setpump\"") != -1){
+    if (message.indexOf("\"params\":true") != -1){
+      pumpstate = true;//turn on Pump
+      pumpControl = true;//point mode control 
+      Serial.println("PUMP ON");
+    }
+    else if(message.indexOf("\"params\":false") != -1){
+      pumpstate = false;//turn off pump
+      pumpControl = false; //return mode auto
+      Serial.println("PUMP OFF");
+    }
+
+    sendPumpStatus();
+  }
+
   //control Led
   if(message.indexOf("\"method\":\"setled\"") != -1){
     if (message.indexOf("\"params\":true") != -1){
@@ -74,6 +108,7 @@ void callback(char* topic, byte* payload, unsigned int length){
       Serial.println("LED OFF");
     }
   }
+
   //control servo
   if(message.indexOf("\"method\":\"setservo\"") != -1){
     int startIndex = message.indexOf("\"params\":") + 9;
@@ -97,6 +132,7 @@ void callback(char* topic, byte* payload, unsigned int length){
 
 //update status of devices
 void updateDevices(){
+  digitalWrite(RELAY_PIN, pumpstate ? HIGH : LOW);
   digitalWrite(LED_PIN, ledstate ? HIGH : LOW);
   myServo.write(servoAngle);
 }
@@ -135,6 +171,8 @@ void reconnectMQTT() {
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   myServo.attach(Servo_PIN);
   dht.begin();
   delay(1000);
@@ -153,27 +191,74 @@ void loop() {
   mqttClient.loop();
 
   updateDevices();
+  //checkTemperature();
   //Send data every 5 seconds
   if (millis() - lastSendTime > TELEMETRY_SEND_INTERVAL) {
-    float temperature = dht.readTemperature(); //read temperature 
-     //random(20, 40); //fretend temperature
-    float humidity = dht.readHumidity(); //read humidity
-    //random(50, 100); //fretend humidity
-    if (isnan(temperature) || isnan(humidity)){
-      Serial.println("Erorr reading from DHT22 sensor!");}
-    else{
-      char payload[100];
-      snprintf(payload, sizeof(payload), "{\"temperature\": %.1f, \"humidity\": %.1f}", temperature, humidity);
-      if(mqttClient.publish("v1/devices/me/telemetry", payload)){
-        Serial.println("Data sent successfully: " + String(payload));
-      }else{
-        Serial.println("Error send data!");
-      }
-    }
+      checkTemperature();
+      sendTelemetryData();
       lastSendTime = millis();
   }
   mqttClient.publish("v1/devices/me/attributes", "{\"test\":\"Hello CoreIoT\"}");
 
   Serial.println("ESP32's still running...");
   delay(1000);
+}
+
+void checkTemperature(){
+  float temperature = dht.readTemperature();
+  if (isnan(temperature)){
+    Serial.println("Erorr reading from DHT22 sensor!");
+    return;
+  }
+
+  if (!pumpControl){
+    if (temperature > TEMP_THRESHOLD){
+      digitalWrite(RELAY_PIN, LOW); //turn on Pump
+      pumpstate = true;
+      if(isBuzzerOn){
+        digitalWrite(BUZZER_PIN, HIGH); //turn on buzzer
+        buzzerStartTime = millis(); //save time turn buzzer
+        isBuzzerOn = true;
+      }  
+      Serial.println("Waning: Temperature exceeds permission! Pump turning on.");
+    }
+    else if (temperature < TEMP_HYSTERESIS) {
+      digitalWrite(RELAY_PIN, HIGH);
+      pumpstate = false;
+      Serial.println("Temperature back normal! Turn off pump.");
+    }
+
+    sendPumpStatus();
+  }
+
+  //Check buzzer sound 1s!
+  if (isBuzzerOn && millis() - buzzerStartTime >= BUZZER_SOUND){
+    digitalWrite(BUZZER_PIN, LOW);
+    isBuzzerOn = false;
+  }
+}
+
+void sendTelemetryData(){
+  float temperature = dht.readTemperature(); //read temperature 
+    //random(20, 40); //fretend temperature
+  float humidity = dht.readHumidity(); //read humidity
+  //random(50, 100); //fretend humidity
+  if (isnan(temperature) || isnan(humidity)){
+    Serial.println("Erorr reading from DHT22 sensor!");}
+  else{
+    char payload[100];
+    snprintf(payload, sizeof(payload), "{\"temperature\": %.1f, \"humidity\": %.1f}", temperature, humidity);
+    if(mqttClient.publish("v1/devices/me/telemetry", payload)){
+      Serial.println("Data sent successfully: " + String(payload));
+    }else{
+      Serial.println("Error send data!");
+    }
+  }
+}
+
+void sendPumpStatus() {
+  char payload[50];
+  snprintf(payload, sizeof(payload), "{\"pumpstate\": %s}", pumpstate ? "true" : "false");
+  mqttClient.publish("v1/devices/me/attributes", payload);
+  Serial.println("Sent pump status to CoreIoT: " + String(payload));
 }
